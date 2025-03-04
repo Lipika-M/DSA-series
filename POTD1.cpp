9d6257b790351e307e97511cd987e96c0e3ab7fc
Lipika Mandal <lipikaam08@gmail.com>
2025-03-05 01:10:50 +0530

    Create POTD1.cpp

---
+++ POTD1.cpp
@@ -0,0 +1,32 @@
+//Partition Array According to Given Pivot
+//https://leetcode.com/problems/partition-array-according-to-given-pivot/
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+      int n=nums.size();
+        vector<int> ans(n, 0);
+        int left = 0, right = n - 1;
+        
+        for (int i = 0, j = n - 1; i < n; ++i, --j) {
+            if (nums[i] < pivot) {
+                ans[left] = nums[i];
+                left++;
+            }
+            
+            if (nums[j] > pivot) {
+                ans[right] = nums[j];
+                right--;
+            }
+        }
+        
+        while (left <= right) {
+            ans[left] = pivot;
+            left++;
+        }
+        
+        return ans;
+    }
+};
+// Approach: created an answer vector of size equal to the given nums then used two pointers to iterate through each element and compare from pivot and the pushing them to ans vector accordingly
+//TC:O(N)
+//SC:O(N)
