98d6ebc5903dc9aaa6fc83623a1b07548ebd9c12
Lipika Mandal <lipikaam08@gmail.com>
2025-03-09 00:21:08 +0530

    Update POTD1.cpp

--- POTD1.cpp
+++ POTD1.cpp
@@ -1,6 +1,7 @@
+//https://leetcode.com/problems/partition-array-according-to-given-pivot/description/
+//https://www.geeksforgeeks.org/problems/longest-bounded-difference-subarray/1
 //Partition Array According to Given Pivot
-//https://leetcode.com/problems/partition-array-according-to-given-pivot/
-class Solution {
+ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
       int n=nums.size();
