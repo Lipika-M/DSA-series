d37e49093d2bd196550f872ed8847775a8b9164f
Lipika Mandal <lipikaam08@gmail.com>
2025-03-09 00:28:00 +0530

    Create POTD7.cpp

---
+++ POTD7.cpp
@@ -0,0 +1,2 @@
+//https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/description/?envType=daily-question&envId=2025-03-08
+//https://www.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1
