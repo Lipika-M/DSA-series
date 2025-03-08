138f25da4992d3742894128c9db1b9958d06f071
Lipika Mandal <lipikaam08@gmail.com>
2025-03-09 00:23:43 +0530

    Create POTD3.cpp

---
+++ POTD3.cpp
@@ -0,0 +1,2 @@
+//https://leetcode.com/problems/count-total-number-of-colored-cells/description/?envType=daily-question&envId=2025-03-05
+//https://www.geeksforgeeks.org/problems/longest-string-chain/1
