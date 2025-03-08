1e61b7715cdf8e03b54be1606b2d63ff2ab8b234
Lipika Mandal <lipikaam08@gmail.com>
2025-03-09 00:22:17 +0530

    Update POTD2.cpp

--- POTD2.cpp
+++ POTD2.cpp
@@ -1,5 +1,6 @@
-//Problem 1:Check if Number is a Sum of Powers of Three
 //https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/
+//https://www.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1
+//Problem 1:Check if Number is a Sum of Powers of Three
 class Solution {
     public:
      bool checkPowersOfThree(int n) {
