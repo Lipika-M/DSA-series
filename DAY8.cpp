11ba4b7e01a843fdcef9c3c5a22bc9e5d7eaf65e
Lipika Mandal <lipikaam08@gmail.com>
2025-03-10 12:25:26 +0530

    Update DAY8.cpp

--- DAY8.cpp
+++ DAY8.cpp
@@ -1,4 +1,5 @@
 //Problem : Rotate array 
+//https://leetcode.com/problems/rotate-array/
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -12,3 +13,19 @@ public:
 //Approach: used pointers to rotate the given array
 //TC:O(N)
 //SC:O(N)
+
+//Problem :Move Zeroes
+//https://leetcode.com/problems/move-zeroes/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       int i=0;
+       for(int j=0;j<nums.size();j++){
+        if (nums[j]!=0){
+            swap(nums[j],nums[i]);
+            i++;
+        } } }
+};
+//Approach:used two pointers to get desired vectors
+//TC:O(N)
+//SC:(N)
