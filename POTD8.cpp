47dcd813ae1827d4b8cb9799759272ca3f0fe73b
Lipika Mandal <lipikaam08@gmail.com>
2025-03-10 12:29:58 +0530

    Update POTD8.cpp

--- POTD8.cpp
+++ POTD8.cpp
@@ -1,2 +1,2 @@
 //https://leetcode.com/problems/alternating-groups-ii/description/?envType=daily-question&envId=2025-03-09
-//https://leetcode.com/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii/description/?envType=daily-question&envId=2025-03-10
+//https://www.geeksforgeeks.org/problems/count-palindrome-sub-strings-of-a-string0652/1
