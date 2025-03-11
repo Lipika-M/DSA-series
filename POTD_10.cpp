231a9cc0d75abc6f678fc9da888ddf05be8071cf
Lipika Mandal <lipikaam08@gmail.com>
2025-03-11 22:07:57 +0530

    Update POTD_10.cpp

--- POTD_10.cpp
+++ POTD_10.cpp
@@ -1,1 +1,2 @@
-
+//https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/?envType=daily-question&envId=2025-03-11
+//https://www.geeksforgeeks.org/problems/count-ways-to-reach-the-nth-stair-1587115620/1
