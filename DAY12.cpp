1e89dae005cf2fbea22b3076248c2be0b93038ab
Lipika Mandal <lipikaam08@gmail.com>
2025-03-13 16:04:01 +0530

    Update DAY12.cpp

--- DAY12.cpp
+++ DAY12.cpp
@@ -76,3 +76,41 @@ public:
 //is the index where the target was found
 //TC:O(logN)
 //SC:O(1)
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        int ans=-1;
+        while(s<=e){
+            if (nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        } 
+         s=0;
+         e=nums.size()-1;
+         mid=s+(e-s)/2;
+         ans=-1;
+         while(s<=e){
+          if(nums[mid]<target){
+            ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ans+1; 
+    }
+};
