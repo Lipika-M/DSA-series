3ebf91fdea5d65412aa9b5ba0782f140b883eaa2
Lipika Mandal <lipikaam08@gmail.com>
2025-03-17 02:29:03 +0530

    Create POTD_15.cpp

---
+++ POTD_15.cpp
@@ -0,0 +1,2 @@
+//https://leetcode.com/problems/minimum-time-to-repair-cars/description/?envType=daily-question&envId=2025-03-16
+//https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
