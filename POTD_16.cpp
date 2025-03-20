be189deb2f69fdfe50bb137eb5477cfaf7c05877
Lipika Mandal <lipikaam08@gmail.com>
2025-03-20 07:27:19 +0530

    Create POTD_16.cpp

---
+++ POTD_16.cpp
@@ -0,0 +1,2 @@
+//https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
+//https://leetcode.com/problems/divide-array-into-equal-pairs/description/?envType=daily-question&envId=2025-03-17
