646b3cb070ebe33b5045788daa4d34618445cd65
Lipika Mandal <lipikaam08@gmail.com>
2025-03-21 19:30:00 +0530

    Update DAY19.cpp

--- DAY19.cpp
+++ DAY19.cpp
@@ -20,3 +20,36 @@ public:
 //approach: used a stack to store the maximum depth of the parenthesis and if closing bracket is encountered then popped it out of the stack
 //tc:O(N)
 //sc:O(M),where m is the max depth
+
+//problem 2:roman to integer
+//https://leetcode.com/problems/roman-to-integer/submissions/1581367528/
+class Solution {
+public:
+    int romanToInt(string s) {
+       unordered_map<char,int>m(7);
+       m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            char ch2=s[i+1];
+            if(m[ch]>=m[ch2]){
+                ans+=m[ch];
+            }
+            else if(m[ch]<m[ch2]){
+                int num=m[ch2]-m[ch];
+                ans+=num;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+//approach:if the value of ith char is less than the value of (i+1)th char then we subtract them and then add to the answer 
+//tc:O(N)
+//sc:O(1)
