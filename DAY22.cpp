77865f339804befc3c6b67b3d98711d70b4c3654
Lipika Mandal <lipikaam08@gmail.com>
2025-03-26 06:46:29 +0530

    Update DAY22.cpp

--- DAY22.cpp
+++ DAY22.cpp
@@ -21,3 +21,34 @@ public:
 //approach: we used a stack to eliminate the valid part and then the remaining stack size would give number of elements to be inserted to make it a valid parentheis
 //TC:O(N)
 //SC:O(M)(where m is the moves required)
+
+//problem:sum of beauty of all substring
+//https://leetcode.com/problems/sum-of-beauty-of-all-substrings/submissions/1586298044/
+class Solution {
+public:
+    int ans = 0;
+    int beautySum(string s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            map<char, int> mp;
+            for (int j = i; j < s.length(); j++) {
+                // counting the frequency of each character
+                mp[s[j]]++;
+
+                int leastFrequent = INT_MAX;
+                int mostFrequent = INT_MIN;
+                // Finding most frequent and least frequent chracter
+                for (auto it : mp) {
+                    leastFrequent = min(leastFrequent, it.second);
+                    mostFrequent = max(mostFrequent, it.second);
+                }
+                ans += mostFrequent - leastFrequent;
+            }
+        }
+
+        return ans;
+    }
+};
+//approach: found all the substring and then added their beauty
+//TC:O(N^2)
+//SC:O(1)
