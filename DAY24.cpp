127b35e95d5d2e3934e406eb5f609914b42b0855
Lipika Mandal <lipikaam08@gmail.com>
2025-03-27 23:15:05 +0530

    Update DAY24.cpp

--- DAY24.cpp
+++ DAY24.cpp
@@ -47,3 +47,37 @@ public:
 //approach:tarversed the dominoes using two pointers and the made the changes simultaneously to the string
 //TC:O(N)
 //SC:O(1)
+
+
+//problem:Partition labels
+//https://leetcode.com/problems/partition-labels/description/?envType=problem-list-v2&envId=string
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char, int> mp;
+
+        // Store the last occurrence of each character
+        for (int i = 0; i < s.length(); i++) {
+            mp[s[i]] = i;
+        }
+
+        vector<int> res;
+        int end = mp[s[0]], start = 0;
+
+        for (int i = 1; i <= s.length(); i++) {
+            if(i <= end) {
+                if(mp[s[i]] > end) {
+                    end = mp[s[i]];
+                }
+            } else {
+                res.push_back(end - start + 1);
+                start = i;
+                end = mp[s[i]];
+            }
+        }
+
+        return res;
+    }
+};
+//TC:O(N)
+//SC:O(N)
