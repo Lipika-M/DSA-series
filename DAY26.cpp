f6e0a75862f72b1f21ae557d572a5df5490aea39
Lipika Mandal <lipikaam08@gmail.com>
2025-03-29 08:59:52 +0530

    Update DAY26.cpp

--- DAY26.cpp
+++ DAY26.cpp
@@ -1,6 +1,5 @@
 //Problem:Search in a linked list
-//https://www.geeksforgeeks.org/problems/insert-a-node-in-doubly-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=insert-a-node-in-doubly-linked-list
-/* Link list node */
+//https://www.geeksforgeeks.org/problems/search-in-linked-list-1664434326/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=search-in-linked-list-1664434326/* Link list node */
 /*
 struct Node
 {
@@ -29,4 +28,54 @@ class Solution {
 };
 //approach:traversed the given linked list to find if key is present or not
 //TC:O(N)
+//SC:O(1)
+
+//Doubly linked list isert at any position
+//https://www.geeksforgeeks.org/problems/insert-a-node-in-doubly-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=insert-a-node-in-doubly-linked-list
+/* a Node of the doubly linked list
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+// Function to insert a new node at given position in doubly linked list.
+class Solution {
+  public:
+    // Function to insert a new node at given position in doubly linked list.
+    Node *addNode(Node *head, int pos, int data) {
+        Node*temp=new Node(data);
+     if(head==NULL){
+         head=temp;
+         return head;
+         }
+     if(pos==0){
+         temp->next=head;
+         head->prev=temp;
+         return head;
+     }
+     Node*p=NULL;
+     Node*curr=head;
+     int i=0;
+     while(i<=pos && curr!=NULL ){
+         p=curr;
+         curr=curr->next;
+         i++;
+     }
+     if (curr==NULL){
+         p->next=temp;
+         temp->prev=p;
+         return head;
+     }
+      p->next=temp;
+     temp->prev=p; 
+     temp->next=curr;
+     curr->prev=temp;
+    
+     return head;
+    }
+};
+//TC:O(N) Where N is the given position
 //SC:O(1)
