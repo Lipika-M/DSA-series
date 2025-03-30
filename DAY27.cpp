7653d71f1c44d69c216f928f2b8029be7208235b
Lipika Mandal <lipikaam08@gmail.com>
2025-03-30 05:52:11 +0530

    Update DAY27.cpp

--- DAY27.cpp
+++ DAY27.cpp
@@ -23,3 +23,35 @@ class Solution {
 //approach:reversed the linked list via recursion
 //tc:O(N)
 //SC:O(1)
+
+//problem:delete a node in a linked list
+//https://www.geeksforgeeks.org/problems/delete-node-in-doubly-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=delete-node-in-doubly-linked-list
+class Solution {
+  public:
+    // Function to delete a node at given position.
+    Node* deleteNode(Node* head, int x) {
+        
+        if (x==1){
+            head=head->next;
+            
+            head->prev=NULL;
+            return head;
+        }
+        int i=1;
+        Node*p=NULL;
+        Node*curr=head;
+        while(i<x){
+            p=curr;
+            curr=curr->next;
+            i++;
+        }
+        p->next=curr->next;
+        if(curr->next!=NULL){
+        curr->next->prev=p;}
+       
+        return head;
+    }
+};
+//Approach:traversed the linked list till the desired node and then deleted it
+//TC:O(X)
+//SC:O(1)
