e50ea0b4f3d32db4554f440e73297f343ec081d6
Lipika Mandal <lipikaam08@gmail.com>
2025-04-01 23:50:15 +0530

    Update DAY29.cpp

--- DAY29.cpp
+++ DAY29.cpp
@@ -45,3 +45,59 @@ ListNode* middleNode(ListNode* head) {
 //approach:we   divided the linked list into left and right part from the middle and the reversed the second part and checked whether the left and the right part are the same
 //TC:O(N)
 //SC:O(1)
+
+
+//problem:count the number of nodes in the cycle
+//https://www.geeksforgeeks.org/problems/find-length-of-loop/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find-length-of-loop
+ 
+class Solution {
+  public:
+   
+    Node* hasCycle(Node *head) {
+        if(head==NULL){
+            return NULL;
+        }
+        Node*slow=head;
+        Node*fast=head;
+       while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){
+                return slow;
+            }
+        }
+        return NULL;
+    }
+    Node* getstart(Node*head){
+        if(head==NULL){
+            return NULL;
+        }
+    
+    Node*intersection=hasCycle(head);
+    if(intersection==NULL){return NULL;}
+       Node*slow=head;
+       while(slow!=intersection){
+         slow=slow->next;
+         intersection=intersection->next;
+         
+       }
+       return slow;
+        }
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+       if(hasCycle(head)==NULL){
+        return  0;   
+       }
+       int count=0;
+       Node*start=getstart(head);
+       Node*temp=start;
+       while(temp->next!=start ){
+           temp=temp->next;
+           count++;
+       }
+       return count+1;
+    }
+};
+//approach:first found if the loop is present or not and then if present then found the start of the loop and then incremented the cound till the loop ends
+//TC:O(N)
+//SC:O(1)
