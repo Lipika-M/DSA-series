c6fd9d4924c7a0ee8692761d2c6cf1bb9ef24473
Lipika Mandal <lipikaam08@gmail.com>
2025-04-01 23:17:58 +0530

    Create DAY29.cpp

---
+++ DAY29.cpp
@@ -0,0 +1,47 @@
+//porblem:palindromic linked list
+//https://leetcode.com/problems/palindrome-linked-list/
+class Solution {
+public:
+ListNode* middleNode(ListNode* head) {
+        if(head==NULL){
+            return head;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+   ListNode* reverse(ListNode* & head){
+       ListNode* p=NULL;
+        ListNode* curr=head; 
+        while(curr!=NULL){
+            ListNode* n=curr->next;
+            curr->next=p;
+            p=curr;
+            curr=n; 
+        }
+        return p;
+         }
+    bool isPalindrome(ListNode* head) {
+        if(head->next==NULL){return true;}
+        ListNode* mid=middleNode( head);
+        ListNode *right=mid->next;
+        mid->next==NULL;
+        right =reverse(right);
+        ListNode*temp1=head;
+        ListNode*temp2=right;
+        while( temp2!=NULL){
+            if(temp1->val !=temp2->val){return false;}
+            else
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        return true;
+    }
+};
+//approach:we   divided the linked list into left and right part from the middle and the reversed the second part and checked whether the left and the right part are the same
+//TC:O(N)
+//SC:O(1)
