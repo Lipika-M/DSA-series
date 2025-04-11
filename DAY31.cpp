2cac56e05692328a99ab7a1b624ca94e900d99c5
Lipika Mandal <lipikaam08@gmail.com>
2025-04-11 22:10:01 +0530

    Update DAY31.cpp

--- DAY31.cpp
+++ DAY31.cpp
@@ -28,3 +28,56 @@ public:
 //APPROACH:deleted the nodes with value equal to given value
 //TC:O(N)
 //SC:O(1)
+
+
+//problem:add two numbers represented by a linked list
+//https://leetcode.com/problems/add-two-numbers/description/
+class Solution {
+public:
+    void insertattail(ListNode*& anshead, ListNode*& anstail, int digit) {
+        ListNode* temp = new ListNode(digit);
+        if (anshead == NULL) {
+            anshead = temp;
+            anstail = temp;
+            return;
+        } else
+            anstail->next = temp;
+        anstail = temp;
+    }
+    ListNode* solve(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        int sum = 0;
+        ListNode* anshead = NULL;
+        ListNode* anstail = anshead;
+
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int val1 = 0;
+            if (l1 != NULL) {
+                val1 = l1->val;
+            }
+            int val2 = 0;
+            if (l2 != NULL) {
+                val2 = l2->val;
+            }
+            sum = val1 + val2 + carry;
+            carry = sum / 10;
+            int digit = sum % 10;
+            insertattail(anshead, anstail, digit);
+
+            if (l1 != NULL) {
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                l2 = l2->next;
+            }
+        }
+        return anshead;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = solve(l1, l2);
+        return ans;
+    }
+};
+//Approach:added numbers of each node and then returned the answer node
+//TC:O(N)
+//SC:O(1)
