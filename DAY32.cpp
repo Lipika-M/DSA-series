ecc59ad8a503b2ea3c9eecd940ca43ace454c731
Lipika Mandal <lipikaam08@gmail.com>
2025-04-12 13:36:33 +0530

    Update DAY32.cpp

--- DAY32.cpp
+++ DAY32.cpp
@@ -38,3 +38,42 @@ public:
 //approach: reversed the first k group and then did the same to the remaining linked list using recursion while the length of the remaining linked list is greater than equal to k
 //TC:O(N)
 //SC:O(1)
+
+//Problem:Remove duplicates from sorted list
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+class Solution {    
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL){return NULL;}
+        unordered_map <int,int>m;
+        ListNode*temp=head;
+        while(temp!=NULL){
+            m[temp->val]++;
+            temp=temp->next;
+        }
+        while(m[head->val]>1){
+            head=head->next;
+            if(head->next==NULL){
+            {
+                if(m[head->val]>1){return NULL;}
+                else{return head;}
+        }}}
+      ListNode*prev=head;
+      ListNode* curr=head->next;
+       while(curr!=NULL){
+        ListNode*next=curr->next;
+        if(m[curr->val]>1){
+            prev->next=next;
+            curr=next;
+        }
+        else{
+            prev=curr;
+            curr=curr->next;
+        }
+       }
+      return head;  
+    }
+};
+//APPROACH:stored the occurance of each node value in a map and then removed the dupliucates
+//TC:O(N)
+//SC:O(1)
