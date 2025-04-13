a68c881659ffeb496734fa27c37e3c267b523407
Lipika Mandal <lipikaam08@gmail.com>
2025-04-13 22:35:07 +0530

    Update DAY33.cpp

--- DAY33.cpp
+++ DAY33.cpp
@@ -59,3 +59,46 @@ void insertattail(Node* &head,Node*&tail,int data){
 //APPROACH: created a mapping from original list to copy and then copied the random pointer
 //TC:O(N)
 //SC:O(N)
+
+//Problem:Rotate list
+//https://leetcode.com/problems/rotate-list/description/
+class Solution {
+public:
+void insertathead(ListNode*&head,int data){
+    ListNode*temp=new ListNode(data);
+    if(head==NULL){
+        head=temp;
+    }
+    temp->next=head;
+    head=temp;
+}
+int length(ListNode*head){
+ListNode*temp=head;
+int l=0;
+while(temp!=NULL){
+l++;
+temp=temp->next;
+}
+return l;
+}
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL||head->next==NULL){
+            return head;
+        }
+   int len=length(head);
+      for(int i=0;i<(k%len);i++){
+        ListNode*temp=head;
+        while(temp->next->next!=NULL){
+            temp=temp->next;
+        }
+        int v=temp->next->val;
+        insertathead(head,v);
+        temp->next=NULL;
+           
+      }
+      return head;  
+    }
+};
+//APPROACH: removed the node from the end and inserted it at the head of the list while updating the head
+//TC:O(N)
+//SC:O(1)
