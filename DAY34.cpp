ed2c4a1de6df913cce5dd49ab6483fc6731294d0
Lipika Mandal <lipikaam08@gmail.com>
2025-04-14 16:28:40 +0530

    Update DAY34.cpp

--- DAY34.cpp
+++ DAY34.cpp
@@ -21,3 +21,35 @@ void SortedStack :: sort()
 //approach:popped the top element and put it in correct position while returning
 //TC:O(N)
 //SC:O(N)
+
+//problem:count good numbers
+//https://leetcode.com/problems/count-good-numbers/description/
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    long long modPow(long long base, long long exp) {
+        long long result = 1;
+        base = base % MOD;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result = (result * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp = exp / 2;
+        }
+        return result;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long even_positions = (n + 1) / 2;
+        long long odd_positions = n / 2;
+        long long res = (modPow(5, even_positions) * modPow(4, odd_positions)) % MOD;
+        return (int)res;
+    }
+};
+//approach:used modulo exponentiation to find the total number of string ,in odd places there are 4 choices while in even there are 5 choices so found the result accordingly
+//TC:O(logN)
+//SC:O(1)
+
+//
